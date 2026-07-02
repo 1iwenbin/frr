1iwenbin/frr

commit eb5d44eb8dcf25a1b328e57d1eabb1f89e3bc59b

Initial revision

isisd/isis_route.c
@@ -1,0 +1,615 @@
+/*
+ * IS-IS Rout(e)ing protocol               - isis_route.c
+ * Copyright (C) 2001,2002   Sampo Saaristo
+ *                           Tampere University of Technology      
+ *                           Institute of Communications Engineering
+ *
+ *                                         based on ../ospf6d/ospf6_route.[ch]
+ *                                         by Yasuhiro Ohara
+ *
+ * This program is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public Licenseas published by the Free 
+ * Software Foundation; either version 2 of the License, or (at your option) 
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for 
+ * more details.
+
+ * You should have received a copy of the GNU General Public License along 
+ * with this program; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <zebra.h>
+#include <net/ethernet.h>
+
+#include "thread.h"
+#include "linklist.h"
+#include "vty.h"
+#include "log.h"
+#include "memory.h"
+#include "prefix.h"
+#include "hash.h"
+#include "if.h"
+#include "table.h"
+
+#include "isis_constants.h"
+#include "isis_common.h"
+#include "dict.h"
+#include "isisd.h"
+#include "isis_misc.h"
+#include "isis_adjacency.h"
+#include "isis_circuit.h"
+#include "isis_tlv.h"
+#include "isis_pdu.h"
+#include "isis_lsp.h"
+#include "isis_spf.h"
+#include "isis_route.h"
+#include "isis_zebra.h"
+
+extern struct isis *isis;
+extern struct thread_master *master;
+
+struct isis_nexthop *
+isis_nexthop_create (struct in_addr *ip, unsigned int ifindex)
+                     
+{
+  struct listnode *node;
+  struct isis_nexthop *nexthop;
+  
+  for (node = listhead (isis->nexthops); node; nextnode (node)) {
+    nexthop = getdata (node);
+    if (nexthop->ifindex != ifindex)
+      continue;
+    if (ip && memcmp (&nexthop->ip, ip, sizeof (struct in_addr)) != 0)
+      continue; 
+
+    nexthop->lock++;
+    return nexthop;
+  }
+  
+  nexthop = XMALLOC (MTYPE_ISIS_NEXTHOP, sizeof (struct isis_nexthop));
+  if (!nexthop) {
+    zlog_err ("ISIS-Rte: isis_nexthop_create: out of memory!");
+  }
+  
+  memset (nexthop, 0, sizeof (struct isis_nexthop));
+  nexthop->ifindex = ifindex;
+  memcpy (&nexthop->ip, ip, sizeof (struct in_addr));
+  listnode_add (isis->nexthops, nexthop);
+  nexthop->lock++;
+
+  return nexthop;
+}
+
+
+void
+isis_nexthop_delete (struct isis_nexthop *nexthop)
+{
+  nexthop->lock--;
+  if (nexthop->lock == 0) {
+    listnode_delete (isis->nexthops, nexthop);
+    XFREE (MTYPE_ISIS_NEXTHOP, nexthop);
+  }
+    
+  return;
+}
+
+int
+nexthoplookup (struct list *nexthops, struct in_addr *ip, 
+               unsigned int ifindex)
+{
+  struct listnode *node;
+  struct isis_nexthop *nh;
+
+  for (node = listhead (nexthops); node; nextnode (node)) {
+    nh = getdata (node);
+    if (!(memcmp (ip, &nh->ip, sizeof (struct in_addr))) && 
+        ifindex == nh->ifindex)
+      return 1;
+  }
+
+  return 0;
+}
+
+void
+nexthop_print (struct isis_nexthop *nh)
+{
+  u_char buf[BUFSIZ];
+  
+  inet_ntop (AF_INET, &nh->ip, buf, BUFSIZ);
+  
+  zlog_info ("      %s %u", buf, nh->ifindex);
+}
+
+void
+nexthops_print (struct list *nhs)
+{
+  struct listnode *node;
+  
+  for (node = listhead(nhs); node; nextnode (node))
+    nexthop_print (getdata (node));
+}
+
+#ifdef HAVE_IPV6
+
+struct isis_nexthop6 *
+isis_nexthop6_new (struct in6_addr *ip6, unsigned int ifindex) 
+{
+  
+  struct isis_nexthop6 *nexthop6;
+  
+  nexthop6 = XMALLOC (MTYPE_ISIS_NEXTHOP6, sizeof (struct isis_nexthop6));
+  if (!nexthop6) {
+    zlog_err ("ISIS-Rte: isis_nexthop_create6: out of memory!");
+  }
+  
+  memset (nexthop6, 0, sizeof (struct isis_nexthop6));
+  nexthop6->ifindex = ifindex;
+  memcpy (&nexthop6->ip6, ip6, sizeof (struct in6_addr));
+  nexthop6->lock++;
+
+  return nexthop6;
+}
+
+struct isis_nexthop6 *
+isis_nexthop6_create (struct in6_addr *ip6, unsigned int ifindex)
+                     
+{
+  struct listnode *node;
+  struct isis_nexthop6 *nexthop6;
+  
+  for (node = listhead (isis->nexthops6); node; nextnode (node)) {
+    nexthop6 = getdata (node);
+    if (nexthop6->ifindex != ifindex)
+      continue;
+    if (ip6 && memcmp (&nexthop6->ip6, ip6, sizeof (struct in6_addr)) != 0)
+      continue; 
+
+    nexthop6->lock++;
+    return nexthop6;
+  }
+  
+  nexthop6 = isis_nexthop6_new (ip6, ifindex);
+
+  return nexthop6;
+}
+
+
+void
+isis_nexthop6_delete (struct isis_nexthop6 *nexthop6)
+{
+
+  nexthop6->lock--;
+  if (nexthop6->lock == 0) {
+    listnode_delete (isis->nexthops6, nexthop6);
+    XFREE (MTYPE_ISIS_NEXTHOP6, nexthop6);
+  }
+    
+  return;
+}
+
+int
+nexthop6lookup (struct list *nexthops6, struct in6_addr *ip6, 
+                unsigned int ifindex)
+{
+  struct listnode *node;
+  struct isis_nexthop6 *nh6;
+
+  for (node = listhead (nexthops6); node; nextnode (node)) {
+    nh6 = getdata (node);
+    if (!(memcmp (ip6, &nh6->ip6, sizeof (struct in6_addr))) && 
+        ifindex == nh6->ifindex)
+      return 1;
+  }
+
+  return 0;
+}
+
+void
+nexthop6_print (struct isis_nexthop6 *nh6)
+{
+  u_char buf[BUFSIZ];
+  
+  inet_ntop (AF_INET6, &nh6->ip6, buf, BUFSIZ);
+  
+  zlog_info ("      %s %u", buf, nh6->ifindex);
+}
+
+void
+nexthops6_print (struct list *nhs6)
+{
+  struct listnode *node;
+  
+  for (node = listhead(nhs6); node; nextnode (node))
+    nexthop6_print (getdata (node));
+}
+
+
+#endif /* HAVE_IPV6 */
+
+void
+adjinfo2nexthop (struct list *nexthops, struct isis_adjacency *adj)
+{
+  struct isis_nexthop *nh;
+  struct listnode *node;
+  struct in_addr *ipv4_addr;
+
+  if (adj->ipv4_addrs == NULL)
+    return;
+  for (node = listhead (adj->ipv4_addrs); node; nextnode (node)) {
+    ipv4_addr = getdata (node);
+    if (!nexthoplookup (nexthops, ipv4_addr, 
+                        adj->circuit->interface->ifindex)) {
+      nh = isis_nexthop_create (ipv4_addr, 
+                                adj->circuit->interface->ifindex);
+      listnode_add (nexthops, nh);
+    }
+  }
+}
+
+#ifdef HAVE_IPV6
+void
+adjinfo2nexthop6 (struct list *nexthops6, struct isis_adjacency *adj)
+{
+  struct listnode *node;
+  struct in6_addr *ipv6_addr;
+  struct isis_nexthop6 *nh6;
+  
+  if (!adj->ipv6_addrs)
+    return;
+
+  for (node = listhead (adj->ipv6_addrs); node; nextnode (node)) {
+    ipv6_addr = getdata (node);
+    if (!nexthop6lookup (nexthops6, ipv6_addr, 
+                         adj->circuit->interface->ifindex)) {
+      nh6 = isis_nexthop6_create (ipv6_addr, 
+                                  adj->circuit->interface->ifindex);
+      listnode_add (nexthops6, nh6);
+    }
+  }
+}
+#endif /* HAVE_IPV6 */
+
+struct isis_route_info *
+isis_route_info_new (uint32_t cost, uint32_t depth, u_char family, 
+                     struct list *adjacencies)
+{
+  struct isis_route_info *rinfo;
+  struct isis_adjacency *adj;
+  struct listnode *node;
+  
+  rinfo = XMALLOC (MTYPE_ISIS_ROUTE_INFO, sizeof (struct isis_route_info));
+  if (!rinfo) {
+    zlog_err ("ISIS-Rte: isis_route_info_new: out of memory!");
+    return NULL;
+  }
+  memset (rinfo, 0, sizeof (struct isis_route_info));
+
+  if (family == AF_INET) {
+    rinfo->nexthops = list_new ();
+    for (node = listhead (adjacencies); node; nextnode (node)) {
+      adj = getdata (node);
+      adjinfo2nexthop (rinfo->nexthops, adj);
+    }
+  }
+#ifdef HAVE_IPV6
+  if (family == AF_INET6) {
+    rinfo->nexthops6 = list_new ();
+    for (node = listhead (adjacencies); node; nextnode (node)) {
+      adj =getdata (node);
+      adjinfo2nexthop6 (rinfo->nexthops6, adj);
+    }
+  }
+    
+#endif /* HAVE_IPV6 */
+
+  rinfo->cost = cost;
+  rinfo->depth = depth;
+      
+  return rinfo;
+}
+
+
+void
+isis_route_info_delete (struct isis_route_info *route_info)
+{
+  
+  if (route_info->nexthops) {
+    route_info->nexthops->del = (void *)isis_nexthop_delete;
+    list_delete (route_info->nexthops);
+  }
+   
+#ifdef HAVE_IPV6
+  if (route_info->nexthops6) {
+      route_info->nexthops6->del = (void *)isis_nexthop6_delete;
+      list_delete (route_info->nexthops6);
+  }
+#endif /* HAVE_IPV6 */
+ 
+  XFREE (MTYPE_ISIS_ROUTE_INFO, route_info);
+}
+
+int
+isis_route_info_same_attrib (struct isis_route_info *new, 
+                             struct isis_route_info *old)
+{
+  if (new->cost != old->cost)
+    return 0;
+  if (new->depth != old->depth)
+    return 0;
+  
+  return 1;
+}
+
+int
+isis_route_info_same (struct isis_route_info *new, struct isis_route_info *old,
+                      u_char family)
+{
+  struct listnode  *node;
+  struct isis_nexthop *nexthop;
+#ifdef HAVE_IPV6
+  struct isis_nexthop6 *nexthop6;
+#endif /* HAVE_IPV6 */
+  if (!isis_route_info_same_attrib (new, old))
+    return 0;
+  
+  if (family == AF_INET) {
+    for (node = listhead (new->nexthops); node; nextnode (node)) {
+      nexthop = (struct isis_nexthop *) getdata (node);
+      if (nexthoplookup (old->nexthops, &nexthop->ip, nexthop->ifindex) == 0)
+        return 0;
+    }
+  
+    for (node = listhead (old->nexthops); node; nextnode (node)) {
+      nexthop = (struct isis_nexthop *) getdata (node);
+      if (nexthoplookup (new->nexthops, &nexthop->ip, nexthop->ifindex) == 0)
+        return 0;
+    }
+  }
+#ifdef HAVE_IPV6
+  else if (family == AF_INET6) {
+    for (node = listhead (new->nexthops6); node; nextnode (node)) {
+      nexthop6 = (struct isis_nexthop6 *) getdata (node);
+      if (nexthop6lookup (old->nexthops6, &nexthop6->ip6, 
+                          nexthop6->ifindex) == 0)
+        return 0;
+    }
+  
+    for (node = listhead (old->nexthops6); node; nextnode (node)) {
+      nexthop6 = (struct isis_nexthop6 *) getdata (node);
+      if (nexthop6lookup (new->nexthops6, &nexthop6->ip6, 
+                          nexthop6->ifindex) == 0)
+        return 0;
+    }
+  }
+#endif /* HAVE_IPV6 */
+
+  return 1;
+}
+
+
+void
+isis_nexthops_merge (struct list *new, struct list *old)
+{
+  struct listnode *node;
+  struct isis_nexthop *nexthop;
+
+  for (node = listhead (new); node; nextnode (node)) {
+    nexthop = (struct isis_nexthop *) getdata (node);
+    if (nexthoplookup (old, &nexthop->ip, nexthop->ifindex))
+      continue;
+    listnode_add (old, nexthop);
+    nexthop->lock++;
+  }
+}
+
+
+#ifdef HAVE_IPV6
+void
+isis_nexthops6_merge (struct list *new, struct list *old)
+{
+  struct listnode *node;
+  struct isis_nexthop6 *nexthop6;
+
+  for (node = listhead (new); node; nextnode (node)) {
+    nexthop6 = (struct isis_nexthop6 *) getdata (node);
+    if (nexthop6lookup (old, &nexthop6->ip6, nexthop6->ifindex))
+      continue;
+    listnode_add (old, nexthop6);
+    nexthop6->lock++;
+  }
+}
+#endif /* HAVE_IPV6 */
+
+void
+isis_route_info_merge (struct isis_route_info *new, 
+                       struct isis_route_info *old, u_char family)
+{
+  
+  if (family == AF_INET) 
+    isis_nexthops_merge (new->nexthops, old->nexthops);
+#ifdef HAVE_IPV6
+  else if (family == AF_INET6) 
+    isis_nexthops6_merge (new->nexthops6, old->nexthops6);
+#endif /* HAVE_IPV6 */
+    
+  return;
+}
+
+
+int
+isis_route_info_prefer_new (struct isis_route_info *new, 
+                            struct isis_route_info *old)
+{
+  
+  if (!CHECK_FLAG (old->flag, ISIS_ROUTE_FLAG_ACTIVE))
+    return 1;
+
+  if (new->cost < old->cost)
+    return 1;
+  
+  return 0;
+}
+
+
+struct isis_route_info *
+isis_route_create (struct prefix *prefix, u_int32_t cost, u_int32_t depth,
+                   struct list *adjacencies, struct isis_area *area)
+{
+  struct route_node *route_node;
+  struct isis_route_info *rinfo_new, *rinfo_old, *route_info = NULL;
+  u_char buff[BUFSIZ];
+  u_char family;
+  
+  family = prefix->family;
+  /* for debugs */
+  prefix2str (prefix, buff, BUFSIZ);
+  
+  rinfo_new = isis_route_info_new (cost, depth, family, adjacencies);
+  if (!rinfo_new) {
+    zlog_err ("ISIS-Rte (%s): isis_route_create: out of memory!",
+	      area->area_tag);
+    return NULL;
+  }
+  
+  if (family == AF_INET)
+    route_node = route_node_get (area->route_table, prefix);  
+#ifdef HAVE_IPV6
+  else if (family == AF_INET6)
+    route_node = route_node_get (area->route_table6, prefix);  
+#endif /* HAVE_IPV6 */
+  else 
+    return NULL;
+  rinfo_old = route_node->info;  
+  if (!rinfo_old) {
+    if (isis->debugs & DEBUG_RTE_EVENTS)
+      zlog_info ("ISIS-Rte (%s) route created: %s", area->area_tag, buff);
+    SET_FLAG(rinfo_new->flag, ISIS_ROUTE_FLAG_ACTIVE);
+    route_node->info = rinfo_new;
+    return rinfo_new;
+  }
+  
+  if (isis->debugs & DEBUG_RTE_EVENTS)
+    zlog_info ("ISIS-Rte (%s) route already exists: %s", area->area_tag, buff);
+  
+  if (isis_route_info_same (rinfo_new, rinfo_old, family)) {
+    if (isis->debugs & DEBUG_RTE_EVENTS)
+      zlog_info ("ISIS-Rte (%s) route unchanged: %s", area->area_tag, buff);
+    isis_route_info_delete (rinfo_new);
+    route_info = rinfo_old;
+  } else if (isis_route_info_same_attrib (rinfo_new, rinfo_old)) {
+    /* merge the nexthop lists */
+    if (isis->debugs & DEBUG_RTE_EVENTS)
+        zlog_info ("ISIS-Rte (%s) route changed (same attribs): %s", 
+                   area->area_tag, buff);
+#ifdef EXTREME_DEBUG
+    zlog_info ("Old nexthops");
+    nexthops6_print (rinfo_old->nexthops6);
+    zlog_info ("New nexthops");
+    nexthops6_print (rinfo_new->nexthops6);
+#endif /* EXTREME_DEBUG */
+    isis_route_info_merge (rinfo_new, rinfo_old, family);
+    isis_route_info_delete (rinfo_new);
+    route_info = rinfo_old;
+  } else {
+    if (isis_route_info_prefer_new (rinfo_new, rinfo_old)) {
+      if (isis->debugs & DEBUG_RTE_EVENTS)
+        zlog_info ("ISIS-Rte (%s) route changed: %s", area->area_tag, buff);
+      isis_route_info_delete (rinfo_old);
+      route_info = rinfo_new;
+    } else {
+      if (isis->debugs & DEBUG_RTE_EVENTS)
+        zlog_info ("ISIS-Rte (%s) route rejected: %s", area->area_tag, buff);
+      isis_route_info_delete (rinfo_new);
+      route_info = rinfo_old;
+    }
+  }
+  
+  SET_FLAG (route_info->flag, ISIS_ROUTE_FLAG_ACTIVE);
+  route_node->info = route_info;
+  
+  return route_info;
+}
+
+void
+isis_route_delete (struct prefix *prefix, struct route_table *table)
+{
+  struct route_node *rode;
+  struct isis_route_info *rinfo;
+  char buff[BUFSIZ];
+
+  /* for log */
+  prefix2str (prefix, buff, BUFSIZ);
+
+
+  rode = route_node_get (table, prefix);
+  rinfo = rode->info;
+
+  if (rinfo == NULL) {
+    if (isis->debugs & DEBUG_RTE_EVENTS)
+      zlog_info ("ISIS-Rte: tried to delete non-existant route %s", buff);
+    return;
+  }
+
+  if (CHECK_FLAG (rinfo->flag, ISIS_ROUTE_FLAG_ZEBRA_SYNC)) {
+    UNSET_FLAG (rinfo->flag, ISIS_ROUTE_FLAG_ACTIVE);
+    if (isis->debugs & DEBUG_RTE_EVENTS)
+      zlog_info ("ISIS-Rte: route delete  %s", buff);
+    isis_zebra_route_update (prefix, rinfo);
+  }
+  isis_route_info_delete (rinfo);
+  rode->info = NULL;
+  
+  return;
+}
+
+int
+isis_route_validate (struct thread *thread)
+{
+  struct isis_area *area;
+  struct route_table *table;
+  struct route_node *rode;
+  struct isis_route_info *rinfo;
+  u_char buff[BUFSIZ];
+#ifdef HAVE_IPV6
+  int v6done = 0;
+#endif
+  area = THREAD_ARG (thread);
+  table = area->route_table;
+#ifdef HAVE_IPV6
+ again:
+#endif
+  for (rode = route_top (table); rode; rode = route_next (rode)) {
+    if (rode->info == NULL)
+      continue;
+    rinfo = rode->info;
+    
+    if (isis->debugs & DEBUG_RTE_EVENTS) {
+      prefix2str (&rode->p, buff, BUFSIZ);
+      zlog_info ("ISIS-Rte (%s): route validate: %s %s %s",
+                 area->area_tag,
+                 (CHECK_FLAG (rinfo->flag, ISIS_ROUTE_FLAG_ZEBRA_SYNC) ?
+                  "sync'ed": "nosync"),
+                 (CHECK_FLAG (rinfo->flag, ISIS_ROUTE_FLAG_ACTIVE) ?
+                  "active": "inactive"), buff);
+    }
+    
+    isis_zebra_route_update (&rode->p, rinfo);
+    if (!CHECK_FLAG (rinfo->flag, ISIS_ROUTE_FLAG_ACTIVE))
+      isis_route_delete (&rode->p, area->route_table);
+  }
+#ifdef HAVE_IPV6
+  if (v6done)
+    return ISIS_OK;
+  table = area->route_table6;
+  v6done = 1;
+  goto again;
+#endif
+
+  return ISIS_OK;
+}

commit f2eabcc7e8ab96263dde855466fa17cbc9eba020

isisd: avoid SPF scheduling for duplicate LSPs
When process_lsp() receives an LSP that compares as LSP_EQUAL, the
LSP has the same sequence number, checksum and contents as the copy
already stored in the LSDB.

The current code still calls lsp_update() in this path.  This refreshes
the in-memory LSP data and schedules SPF through lsp_update_data(),
even though the topology information has not changed.

In dense L2 deployments, periodic CSNP/PSNP exchanges can cause the same
LSP to be received repeatedly from multiple neighbors.  Each duplicate
LSP then triggers an unnecessary L2 SPF run, creating sustained control
plane churn after convergence.

For LSP_EQUAL, keep the existing LSDB contents and refresh only the
remaining lifetime.  Newer LSPs still go through lsp_update() and
continue to schedule SPF as before.  LSP_NEWER, LSP_OLDER and purge
handling are unaffected.

Fixes: eb5d44eb8d ("Initial revision")

Also:
- Comment out SR-DBG debug logs (spam reduction, 6 files)
- Remove IS_DEBUG_SPF_EVENTS guard from SPF schedule/run logs
- Add ISIS-SPF trigger: LSP-ID-annotated debug logs in lsp_update,
  lsp_insert, lsp_destroy and lsp_aged_out paths

isisd/isis_lsp.c
@@ -142,6 +142,8 @@ static void lsp_destroy(struct isis_lsp *lsp)
 		}
 	}
 
+	zlog_debug("ISIS-SPF trigger: lsp_destroy %s L%d",
+		   rawlspid_print(lsp->hdr.lsp_id), lsp->level);
 	isis_spf_schedule(lsp->area, lsp->level);
 
 	if (lsp->pdu)
@@ -345,6 +347,8 @@ void lsp_inc_seqno(struct isis_lsp *lsp, uint32_t seqno)
 	lsp->hdr.seqno = newseq;
 
 	lsp_pack_pdu(lsp);
+	zlog_debug("ISIS-SPF trigger: lsp_inc_seqno %s L%d",
+		   rawlspid_print(lsp->hdr.lsp_id), lsp->level);
 	isis_spf_schedule(lsp->area, lsp->level);
 	isis_te_lsp_event(lsp, LSP_INC);
 }
@@ -583,6 +587,8 @@ void lsp_update(struct isis_lsp *lsp, struct isis_lsp_hdr *hdr,
 	}
 
 	if (lsp->hdr.seqno) {
+		zlog_debug("ISIS-SPF trigger: lsp_update %s L%d",
+			   rawlspid_print(lsp->hdr.lsp_id), lsp->level);
 		isis_spf_schedule(lsp->area, lsp->level);
 		isis_te_lsp_event(lsp, LSP_UPD);
 	}
@@ -667,6 +673,8 @@ void lsp_insert(struct lspdb_head *head, struct isis_lsp *lsp)
 {
 	lspdb_add(head, lsp);
 	if (lsp->hdr.seqno) {
+		zlog_debug("ISIS-SPF trigger: lsp_insert %s L%d",
+			   rawlspid_print(lsp->hdr.lsp_id), lsp->level);
 		isis_spf_schedule(lsp->area, lsp->level);
 		isis_te_lsp_event(lsp, LSP_ADD);
 	}
@@ -963,8 +971,8 @@ static void lsp_build_ext_reach_ipv6(struct isis_lsp *lsp,
 	struct route_table *er_table =
 		get_ext_reach(area, AF_INET6, lsp->level);
 
-	zlog_debug("SR-DBG: lsp_build_ext_reach_ipv6 level=%d table=%p",
-		   lsp->level, (void *)er_table);
+	/* zlog_debug("SR-DBG: lsp_build_ext_reach_ipv6 level=%d table=%p",
+	 * 	   lsp->level, (void *)er_table); */
 
 	if (!er_table)
 		return;
@@ -989,8 +997,8 @@ static void lsp_build_ext_reach_ipv6(struct isis_lsp *lsp,
 			if (area->srdb.enabled)
 				pcfg = isis_sr_cfg_prefix_find(area, p);
 
-			zlog_debug("SR-DBG: lsp_build_ipv6 %pFX pcfg=%p",
-				   p, (void *)pcfg);
+/* zlog_debug("SR-DBG: lsp_build_ipv6 %pFX pcfg=%p",
+		 * 	   p, (void *)pcfg); */
 
 			isis_tlvs_add_ipv6_reach(lsp->tlvs,
 						 isis_area_ipv6_topology(area),

isisd/isis_pdu.c
@@ -1333,9 +1333,10 @@ dontcheckadj:
 		/* 7.3.15.1 e) 2) LSP equal to the one in db */
 		else if (comp == LSP_EQUAL) {
 			isis_tx_queue_del(circuit->tx_queue, lsp);
-			lsp_update(lsp, &hdr, tlvs, circuit->rcv_stream,
-				   circuit->area, level, false);
-			tlvs = NULL;
+			/* The LSP contents are unchanged.  Refresh the remaining
+			 * lifetime only; updating the LSDB copy would
+			 * unnecessarily schedule SPF. */
+			lsp->hdr.rem_lifetime = hdr.rem_lifetime;
 			if (circuit->circ_type != CIRCUIT_T_BROADCAST)
 				ISIS_SET_FLAG(lsp->SSNflags, circuit);
 		}

isisd/isis_route.c
@@ -466,12 +466,12 @@ static void isis_route_update(struct isis_area *area, struct prefix *prefix,
 	if (area == NULL)
 		return;
 
-	zlog_debug("SR-DBG: route_update %pFX active=%d synced=%d sr.present=%d sr.label=%u",
-		   prefix,
-		   CHECK_FLAG(route_info->flag, ISIS_ROUTE_FLAG_ACTIVE) ? 1 : 0,
-		   CHECK_FLAG(route_info->flag, ISIS_ROUTE_FLAG_ZEBRA_SYNCED) ? 1 : 0,
-		   route_info->sr.present ? 1 : 0,
-		   route_info->sr.label);
+	/* zlog_debug("SR-DBG: route_update %pFX active=%d synced=%d sr.present=%d sr.label=%u",
+	 * 	   prefix,
+	 * 	   CHECK_FLAG(route_info->flag, ISIS_ROUTE_FLAG_ACTIVE) ? 1 : 0,
+	 * 	   CHECK_FLAG(route_info->flag, ISIS_ROUTE_FLAG_ZEBRA_SYNCED) ? 1 : 0,
+	 * 	   route_info->sr.present ? 1 : 0,
+	 * 	   route_info->sr.label); */
 
 	if (CHECK_FLAG(route_info->flag, ISIS_ROUTE_FLAG_ACTIVE)) {
 		if (CHECK_FLAG(route_info->flag, ISIS_ROUTE_FLAG_ZEBRA_SYNCED))

isisd/isis_spf.c
@@ -1279,14 +1279,13 @@ lspfragloop:
 
 			/* Parse list of Prefix-SID subTLVs */
 			has_valid_psid = false;
-			zlog_debug("SR-DBG: spf_ipv6 %pFX depth=%d subtlvs=%p psids=%p %s",
-				   &ip_info.dest, depth,
-				   (void *)r->subtlvs,
-				   r->subtlvs
-					   ? (void *)r->subtlvs->prefix_sids
-						     .head
-					   : NULL,
-				   rawlspid_print(lsp->hdr.lsp_id));
+			/* zlog_debug("SR-DBG: spf_ipv6 %pFX depth=%d subtlvs=%p psids=%p %s",
+			 * 	   &ip_info.dest, depth,
+			 * 	   (void *)r->subtlvs,
+			 * 	   r->subtlvs
+			 * 		   ? (void *)r->subtlvs->prefix_sids.head
+			 * 		   : NULL,
+			 * 	   rawlspid_print(lsp->hdr.lsp_id)); */
 			if (r->subtlvs) {
 				for (struct isis_item *i =
 					     r->subtlvs->prefix_sids.head;
@@ -2141,9 +2140,8 @@ static void isis_run_spf_cb(struct thread *thread)
 	isis_area_delete_backup_adj_sids(area, level);
 	isis_area_invalidate_routes(area, level);
 
-	if (IS_DEBUG_SPF_EVENTS)
-		zlog_debug("ISIS-SPF (%s) L%d SPF needed, periodic SPF",
-			   area->area_tag, level);
+	zlog_debug("ISIS-SPF (%s) L%d SPF run (scheduled)",
+		   area->area_tag, level);
 
 	if (area->ip_circuits) {
 		isis_run_spf_with_protection(
@@ -2203,11 +2201,9 @@ int _isis_spf_schedule(struct isis_area *area, int level,
 	assert(diff >= 0);
 	assert(area->is_type & level);
 
-	if (IS_DEBUG_SPF_EVENTS) {
-		zlog_debug(
-			"ISIS-SPF (%s) L%d SPF schedule called, lastrun %d sec ago Caller: %s %s:%d",
+	zlog_debug(
+			"ISIS-SPF (%s) L%d SPF schedule: lastrun=%ds ago reason=%s (from %s:%d)",
 			area->area_tag, level, diff, func, file, line);
-	}
 
 	THREAD_OFF(area->t_rlfa_rib_update);
 	if (area->spf_delay_ietf[level - 1]) {
@@ -2252,9 +2248,8 @@ int _isis_spf_schedule(struct isis_area *area, int level,
 	thread_add_timer(master, isis_run_spf_cb, isis_run_spf_arg(area, level),
 			 timer, &area->spf_timer[level - 1]);
 
-	if (IS_DEBUG_SPF_EVENTS)
-		zlog_debug("ISIS-SPF (%s) L%d SPF scheduled %ld sec from now",
-			   area->area_tag, level, timer);
+	zlog_debug("ISIS-SPF (%s) L%d SPF scheduled %ld sec from now",
+		   area->area_tag, level, timer);
 
 	return ISIS_OK;
 }

isisd/isis_sr.c
@@ -90,19 +90,19 @@ struct isis_sr_block *isis_sr_find_srgb(struct lspdb_head *lspdb,
 
 	lsp = isis_root_system_lsp(lspdb, sysid);
 	if (!lsp) {
-		zlog_debug("SR-DBG: find_srgb(%pLS) LSP NOT FOUND", sysid);
+		/* zlog_debug("SR-DBG: find_srgb(%pLS) LSP NOT FOUND", sysid); */
 		return NULL;
 	}
 
 	/* Primary: Router Capability SRGB (RFC 9666 Step 4). */
 	if (lsp->tlvs->router_cap
 	    && lsp->tlvs->router_cap->srgb.range_size > 0) {
-		zlog_debug("SR-DBG: find_srgb(%pLS) → RouterCap SRGB [%u,%u) flags=0x%x",
-			   sysid,
-			   lsp->tlvs->router_cap->srgb.lower_bound,
-			   lsp->tlvs->router_cap->srgb.lower_bound
-				   + lsp->tlvs->router_cap->srgb.range_size,
-			   lsp->tlvs->router_cap->srgb.flags);
+		/* zlog_debug("SR-DBG: find_srgb(%pLS) → RouterCap SRGB [%u,%u) flags=0x%x",
+		 * 	   sysid,
+		 * 	   lsp->tlvs->router_cap->srgb.lower_bound,
+		 * 	   lsp->tlvs->router_cap->srgb.lower_bound
+		 * 		   + lsp->tlvs->router_cap->srgb.range_size,
+		 * 	   lsp->tlvs->router_cap->srgb.flags); */
 		return &lsp->tlvs->router_cap->srgb;
 	}
 
@@ -117,8 +117,8 @@ struct isis_sr_block *isis_sr_find_srgb(struct lspdb_head *lspdb,
 	    && lsp->tlvs->area_proxy->has_area_sid) {
 		static struct isis_sr_block area_sid_srgb;
 
-		zlog_debug("SR-DBG: find_srgb(%pLS) → AreaSID fallback value=%u",
-			   sysid, lsp->tlvs->area_proxy->area_sid_value);
+		/* zlog_debug("SR-DBG: find_srgb(%pLS) → AreaSID fallback value=%u",
+		 * 	   sysid, lsp->tlvs->area_proxy->area_sid_value); */
 
 		area_sid_srgb.lower_bound =
 			lsp->tlvs->area_proxy->area_sid_value;
@@ -222,8 +222,8 @@ mpls_label_t sr_prefix_out_label(struct lspdb_head *lspdb, int family,
 	 */
 	if ((family == AF_INET && !IS_SR_IPV4(nh_srgb))
 	    || (family == AF_INET6 && !IS_SR_IPV6(nh_srgb))) {
-		zlog_debug("SR-DBG: out_label(%pLS) → INVALID (AF check fail, flags=0x%x)",
-			   nh_sysid, nh_srgb->flags);
+		/* zlog_debug("SR-DBG: out_label(%pLS) → INVALID (AF check fail, flags=0x%x)",
+		 * 	   nh_sysid, nh_srgb->flags); */
 		return MPLS_INVALID_LABEL;
 	}
 

isisd/isis_tlvs.c
@@ -3334,9 +3334,9 @@ static int pack_item_ipv6_reach(struct isis_item *i, struct stream *s,
 	control |= r->external ? ISIS_IPV6_REACH_EXTERNAL : 0;
 	control |= r->subtlvs ? ISIS_IPV6_REACH_SUBTLV : 0;
 
-	zlog_debug("SR-DBG: PACK ipv6 %pFX subtlvs=%p flag=%s",
-		   &r->prefix, (void *)r->subtlvs,
-		   r->subtlvs ? "SET" : "NOT SET");
+	/* zlog_debug("SR-DBG: PACK ipv6 %pFX subtlvs=%p flag=%s",
+	 * 	   &r->prefix, (void *)r->subtlvs,
+	 * 	   r->subtlvs ? "SET" : "NOT SET"); */
 
 	stream_putc(s, control);
 	stream_putc(s, r->prefix.prefixlen);
@@ -3439,8 +3439,8 @@ static int unpack_item_ipv6_reach(uint16_t mtid, uint8_t len, struct stream *s,
 			rv->subtlvs = NULL;
 		}
 	} else {
-		zlog_debug("SR-DBG: TLV decode: %pFX IPv6 reach SUBTLV flag NOT SET",
-			   &rv->prefix);
+		/* zlog_debug("SR-DBG: TLV decode: %pFX IPv6 reach SUBTLV flag NOT SET",
+		 * 	   &rv->prefix); */
 	}
 
 	append_item(items, (struct isis_item *)rv);

isisd/isis_zebra.c
@@ -177,10 +177,10 @@ static int isis_zebra_add_nexthops(struct isis *isis, struct list *nexthops,
 	for (ALL_LIST_ELEMENTS_RO(nexthops, node, nexthop)) {
 		struct zapi_nexthop *api_nh;
 
-		zlog_debug("SR-DBG:   nh[%d] family=%d ip=%pI6 sr.present=%d label_stack=%p mpls_lsp=%d type=%d",
-			   count, nexthop->family, &nexthop->ip.ipv6,
-			   nexthop->sr.present, (void *)nexthop->label_stack,
-			   mpls_lsp, type);
+		/* zlog_debug("SR-DBG:   nh[%d] family=%d ip=%pI6 sr.present=%d label_stack=%p mpls_lsp=%d type=%d",
+		 * 	   count, nexthop->family, &nexthop->ip.ipv6,
+		 * 	   nexthop->sr.present, (void *)nexthop->label_stack,
+		 * 	   mpls_lsp, type); */
 
 		if (count >= MULTIPATH_NUM)
 			break;
@@ -227,7 +227,7 @@ static int isis_zebra_add_nexthops(struct isis *isis, struct list *nexthops,
 		} else if (mpls_lsp) {
 			switch (type) {
 			case ISIS_NEXTHOP_MAIN:
-				zlog_debug("SR-DBG:   SKIP: MAIN nh without sr/label_stack");
+				/* zlog_debug("SR-DBG:   SKIP: MAIN nh without sr/label_stack"); */
 				continue;
 			case ISIS_NEXTHOP_BACKUP:
 				/*
@@ -351,9 +351,9 @@ void isis_zebra_prefix_sid_install(struct isis_area *area,
 	sr_debug("ISIS-Sr (%s): update label %u for prefix %pFX",
 		 area->area_tag, psid->label, prefix);
 
-	zlog_debug("SR-DBG: prefix_sid_install %pFX label=%u nh_count=%u sr.present=%d",
-		   prefix, psid->label, listcount(rinfo->nexthops),
-		   psid->present);
+	/* zlog_debug("SR-DBG: prefix_sid_install %pFX label=%u nh_count=%u sr.present=%d",
+	 * 	   prefix, psid->label, listcount(rinfo->nexthops),
+	 * 	   psid->present); */
 
 	/* Prepare message. */
 	memset(&zl, 0, sizeof(zl));